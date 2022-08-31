88884d84d458543f753b25fb9f295cb30b8d7afa
anthony4tner <adfortner@protonmail.com>
2022-08-31 16:47:20 -0700

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,22 @@
 import * as React from "react"
 // import { Parallax } from "@react-spring/parallax"
-// import Layout from "../src/components/layout"
+import Layout from "../src/components/layout"
 // import Hero from "../src/components/hero"
 // import Projects from "../src/components/projects"
 // import About from "../src/components/about"
 // import Contact from "../src/components/contact"
 
 const Illiac = () => (
-  // <Layout>
-  //   <Parallax pages={5}>
-  //     {/* <div dangerouslySetInnerHTML={{ __html: html }} />
-  //     <style jsx>{`
-  //       h1,
-  //       div {
-  //         text-align: center;
-  //       }
-  //     `}</style> */}
-  //     <Hero offset={0} factor={1} />
-  //     <Projects offset={1} factor={2} />
-  //     <About offset={3} factor={1} />
-  //     <Contact offset={4} factor={1} />
-  //   </Parallax>
-  // </Layout>
-  <div>TEST</div>
+  <Layout>
+    <div>Test</div>
+    {/* <Parallax pages={5}>
+      <Hero offset={0} factor={1} />
+      <Projects offset={1} factor={2} />
+      <About offset={3} factor={1} />
+      <Contact offset={4} factor={1} />
+    </Parallax> */}
+  </Layout>
+  // <div>TEST</div>
 )
 
 export default Illiac
